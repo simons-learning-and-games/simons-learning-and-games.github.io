f9d17e41c6ca25795a3cadfb379c73ca1a66f665
phoebkal <phoekalogiannis@gmail.com>
2022-02-15 19:25:36 +0200

    add

--- add_to_csv.py
+++ add_to_csv.py
@@ -1,37 +1,21 @@
 import pandas as pd
 import os
 
-#fname = input("Choose schedule to append to: (seminar/reading-group-1/reading-group-2) :")
-#date = input("Date in format dd/mm/yyyy : ")
-#time = input("Time of the talk in format HH:MM : ")
-#speaker = input("Name and Surname of the speaker: ")
-#affiliation = input("Affiliation of the speaker: ")
-#url = input("Homepage of the speaker: ")
-#title = input("Title of the talk: ")
-#abstract = input("Abstract of the talk: ")
-#bio = input("Short bio of the speaker: ")
-#related = input("Papers connected to the talk (List all titles separated by semicolon(;) :")
-#location = input("Location of the talk: ")
-#imgfile = input("Name of the image file you added to the img folder with a portrat of the speaker :")
-#zoom = input("Zoom link of the talk: ")
-#
-date = '10/02/22'
-time = '03:30'
-speaker = 'foo'
-url = 'foo.gr'
-affiliation = 'ntua'
-bio = 'as'
-abstract = 'as if'
-related = 'ssss'
-zoom = 'aaaaaa'
-title = 'bar foo spam'
-location = 'spam'
-imgfile = 'spam.png'
-fname = 'seminar'
-row = { 'date': date, 'time':time, 'speaker':speaker, 'url' : url,	\
-    'affiliation' : affiliation, 'title' : title, 'bio' : bio,  \
-    'abstract' : abstract, 'imgfile' : imgfile, 'location' :location, \
-    'zoom' : zoom,	'connected-papers' : related }
+fname = input("Choose schedule to append to: (seminar/reading-group-1/reading-group-2) :")
+date = input("Date in format dd/mm/yyyy : ")
+time = input("Time of the talk in format HH:MM : ")
+speaker = input("Name and Surname of the speaker: ")
+affiliation = input("Affiliation of the speaker: ")
+url = input("Homepage of the speaker: ")
+title = input("Title of the talk: ")
+abstract = input("Abstract of the talk: ")
+bio = input("Short bio of the speaker: ")
+related = input("Papers connected to the talk (List all titles separated by semicolon(;) :")
+location = input("Location of the talk: ")
+imgfile = input("Name of the image file you added to the img folder with a portrat of the speaker :")
+zoom = input("Zoom link of the talk: ")
+
+
 
 
 print(row)
